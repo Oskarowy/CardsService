4f4a8b2544d699b386f7a256a161bef6f12d12ea
Oskar Białek <os.bialek@gmail.com>
2025-01-14 21:25:24 +0100

    Red Action1_ShouldBeAllowed_ForPrepaidCard_IfCardIsActive test created

---
+++ CardsService.UnitTests/ActionServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using CardsService.Model;
+using CardsService.Services;
+
+namespace CardsService.UnitTests
+{
+    public class ActionServiceTests
+    {
+        [Fact]
+        public void Action1_ShouldBeAllowed_ForPrepaidCard_IfCardIsActive()
+        {
+            string action1name = "ACTION1";
+
+            var service = new ActionService();
+            var cardDetails = new CardDetails("123", CardType.Prepaid, CardStatus.Active, true);
+
+            List<string> allowedActions = service.GetAllowedActions(cardDetails);
+
+            Assert.NotNull(allowedActions);
+            Assert.Contains(action1name, allowedActions);
+        }
+    }
+}
