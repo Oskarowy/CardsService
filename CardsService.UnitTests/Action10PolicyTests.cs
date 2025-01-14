f283d2a0369d7dda9dc1074ec31fc5dde90e9fc8
Oskar Białek <os.bialek@gmail.com>
2025-01-15 00:45:15 +0100

    created Action10PolicyTests

---
+++ CardsService.UnitTests/Action10PolicyTests.cs
@@ -0,0 +1,42 @@
+﻿using CardsService.Model;
+using CardsService.Policies;
+using CardsService.Services;
+
+namespace CardsService.UnitTests
+{
+    public class Action10PolicyTests
+    {
+        private readonly Action10Policy _action10policy = new Action10Policy();
+        private string _cardNumber = "12345";
+
+        public Action10PolicyTests()
+        {
+        }
+
+        [Theory]
+        [MemberData(nameof(CardsMatrixProvider.AllCardsCollection), MemberType = typeof(CardsMatrixProvider))]
+        public void Action10_Allow_ForAnyCardType_IfCardIsActiveInActiveOrdered_NoMatterIfThereIsPIN(CardType cardType, CardStatus cardStatus, bool isPinSet)
+        {
+            var availableStatuses = new List<CardStatus> { CardStatus.Active, CardStatus.Inactive, CardStatus.Ordered };
+
+            if (!availableStatuses.Contains(cardStatus)) return;
+
+            var testedCard = new CardDetails(_cardNumber, cardType, cardStatus, isPinSet);
+
+            Assert.True(_action10policy.IsAllowed(testedCard));
+        }
+
+        [Theory]
+        [MemberData(nameof(CardsMatrixProvider.AllCardsCollection), MemberType = typeof(CardsMatrixProvider))]
+        public void Action10_Deny_ForAnyCardType_IfCardIsRestrictedExpiredClosedBlocked_NoMatterIfThereIsPIN(CardType cardType, CardStatus cardStatus, bool isPinSet)
+        {
+            var availableStatuses = new List<CardStatus> { CardStatus.Restricted, CardStatus.Expired, CardStatus.Closed, CardStatus.Blocked };
+
+            if (!availableStatuses.Contains(cardStatus)) return;
+
+            var cardDetails = new CardDetails(_cardNumber, cardType, cardStatus, isPinSet);
+
+            Assert.False(_action10policy.IsAllowed(cardDetails));
+        }
+    }
+}
