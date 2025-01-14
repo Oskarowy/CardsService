d04c21eb11859bbe0c13d820a4a41f9abd17c3b3
Oskar Białek <os.bialek@gmail.com>
2025-01-14 21:46:04 +0100

    refactor and introduce the ActionPolicy concept

--- CardsService.UnitTests/ActionServiceTests.cs
+++ CardsService.UnitTests/ActionServiceTests.cs
@@ -5,19 +5,16 @@ namespace CardsService.UnitTests
 {
     public class ActionServiceTests
     {
+        private readonly ActionService _actionService = new ActionService();
+        private readonly Action1Policy _policy = new Action1Policy();
+
         [Theory]
         [InlineData(CardStatus.Active)]
         public void Action1_Allow_ForPrepaidCard_IfCardIsActive(CardStatus cardStatus)
         {
-            string action1name = "ACTION1";
-
-            var service = new ActionService();
             var cardDetails = new CardDetails("123", CardType.Prepaid, cardStatus, true);
 
-            List<string> allowedActions = service.GetAllowedActions(cardDetails);
-
-            Assert.NotNull(allowedActions);
-            Assert.Contains(action1name, allowedActions);
+            Assert.True(_policy.IsAllowed(cardDetails));
         }
 
         [Theory]
@@ -29,14 +26,9 @@ namespace CardsService.UnitTests
         [InlineData(CardStatus.Closed)]
         public void Action1_Deny_ForPrepaidCard_IfCardIsNotActive(CardStatus cardStatus)
         {
-            string action1name = "ACTION1";
-
-            var service = new ActionService();
             var cardDetails = new CardDetails("123", CardType.Prepaid, cardStatus, true);
 
-            List<string> allowedActions = service.GetAllowedActions(cardDetails);
-
-            Assert.DoesNotContain(action1name, allowedActions);
+            Assert.False(_policy.IsAllowed(cardDetails));
         }
     }
 }
