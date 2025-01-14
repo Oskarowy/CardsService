98761d1a12500a38780e2160b5e92aa58ee79252
Oskar Białek <os.bialek@gmail.com>
2025-01-14 23:50:01 +0100

    create Action4_Allow_ForAnyCardType_ForAnyCardStatus_NoMatterIfThereIsPIN

---
+++ CardsService.UnitTests/Action4PolicyTests.cs
@@ -0,0 +1,25 @@
+﻿using CardsService.Model;
+using CardsService.Policies;
+using CardsService.Services;
+
+namespace CardsService.UnitTests
+{
+    public class Action4PolicyTests
+    {
+        private readonly Action4Policy _action4policy = new Action4Policy();
+        private string _cardNumber = "12345";
+
+        public Action4PolicyTests()
+        {
+        }
+
+        [Theory]
+        [MemberData(nameof(CardsMatrixProvider.AllCardsCollection), MemberType = typeof(CardsMatrixProvider))]
+        public void Action4_Allow_ForAnyCardType_ForAnyCardStatus_NoMatterIfThereIsPIN(CardType cardType, CardStatus cardStatus, bool isPinSet)
+        {
+            var testedCard = new CardDetails(_cardNumber, cardType, cardStatus, isPinSet);
+
+            Assert.True(_action4policy.IsAllowed(testedCard));
+        }
+    }
+}
