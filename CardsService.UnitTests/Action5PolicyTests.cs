34e4aa9eb8f5430803fa0f52b29787a7b8ec470e
Oskar Białek <os.bialek@gmail.com>
2025-01-15 00:00:57 +0100

    created Action5_Deny_ForDebitAndPrepaidCard_ForEveryCardStatus_NoMatterIfThereIsPIN test

--- CardsService.UnitTests/Action5PolicyTests.cs
+++ CardsService.UnitTests/Action5PolicyTests.cs
@@ -23,5 +23,16 @@ namespace CardsService.UnitTests
 
             Assert.True(_action5policy.IsAllowed(testedCard));
         }
+
+        [Theory]
+        [MemberData(nameof(CardsMatrixProvider.AllCardsCollection), MemberType = typeof(CardsMatrixProvider))]
+        public void Action5_Deny_ForDebitAndPrepaidCard_ForEveryCardStatus_NoMatterIfThereIsPIN(CardType cardType, CardStatus cardStatus, bool isPinSet)
+        {
+            if (cardType == CardType.Credit) return;
+
+            var testedCard = new CardDetails(_cardNumber, cardType, cardStatus, isPinSet);
+
+            Assert.False(_action5policy.IsAllowed(testedCard));
+        }
     }
 }
