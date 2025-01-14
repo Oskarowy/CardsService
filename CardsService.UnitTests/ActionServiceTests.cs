7485f05244084effe92bed8fff072b8dcfd1d9da
Oskar Białek <os.bialek@gmail.com>
2025-01-14 22:26:49 +0100

    injected policies list into service + implement Action2_ShouldBeReturnedByService_IfPolicyIsAllowed logic

--- CardsService.UnitTests/ActionServiceTests.cs
+++ CardsService.UnitTests/ActionServiceTests.cs
@@ -6,7 +6,8 @@ namespace CardsService.UnitTests
 {
     public class ActionServiceTests
     {
-        private readonly ActionService _actionService = new ActionService();
+        private readonly ActionService _actionService;
+        private readonly List<IActionPolicy> _policies;
         private readonly Action1Policy _action1policy = new Action1Policy();
         private readonly Action2Policy _action2policy = new Action2Policy();
         private readonly CardDetails _prepaidActiveCard;
@@ -14,6 +15,11 @@ namespace CardsService.UnitTests
 
         public ActionServiceTests()
         {
+            _policies = new List<IActionPolicy> 
+            {
+                _action1policy,_action2policy
+            };
+            _actionService = new ActionService(_policies);
             _prepaidActiveCard = new CardDetails("123", CardType.Prepaid, CardStatus.Active, true);
             _prepaidInactiveCard = new CardDetails("123", CardType.Prepaid, CardStatus.Inactive, true);
         }
@@ -58,7 +64,7 @@ namespace CardsService.UnitTests
         [Fact]
         public void Action2_ShouldBeReturnedByService_IfPolicyIsAllowed()
         {
-            var allowedActions = _actionService.GetAllowedActions(_prepaidActiveCard);
+            var allowedActions = _actionService.GetAllowedActions(_prepaidInactiveCard);
 
             Assert.True(_action2policy.IsAllowed(_prepaidInactiveCard));
             Assert.Contains(_action2policy.ActionName, allowedActions);
