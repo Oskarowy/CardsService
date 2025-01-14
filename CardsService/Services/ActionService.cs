34aaebe904abd2e0e0ac7b5c9e9d7e260c1fea76
Oskar Białek <os.bialek@gmail.com>
2025-01-14 21:29:32 +0100

    test implementation

---
+++ CardsService/Services/ActionService.cs
@@ -0,0 +1,16 @@
+﻿namespace CardsService.Services;
+
+public class ActionService
+{
+    public ActionService()
+    {
+       
+    }
+
+    public List<string> GetAllowedActions(CardDetails cardDetails) 
+    {
+        string action1name = "ACTION1";
+
+        return new List<string> { action1name };
+    }
+}
