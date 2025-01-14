aaf17f2abb13129300e15389e29aa2154c73ad3e
Oskar Białek <os.bialek@gmail.com>
2025-01-15 00:48:57 +0100

    implemented logic to pass Action13 tests

---
+++ CardsService/Policies/Action13Policy.cs
@@ -0,0 +1,18 @@
+﻿using CardsService.Model;
+using CardsService.Services;
+using System.Reflection.Metadata.Ecma335;
+
+namespace CardsService.Policies
+{
+    public class Action13Policy : IActionPolicy
+    {
+        public string ActionName => "ACTION13";
+
+        public bool IsAllowed(CardDetails cardDetails)
+        {
+            var availableStatuses = new List<CardStatus> { CardStatus.Active, CardStatus.Inactive, CardStatus.Ordered };
+
+            return availableStatuses.Contains(cardDetails.CardStatus);
+        }
+    }
+}
