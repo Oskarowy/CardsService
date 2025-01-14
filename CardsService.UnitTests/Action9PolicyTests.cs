66c3bf214ef61b916fb600a8f4a64a1d88d14322
Oskar Białek <os.bialek@gmail.com>
2025-01-14 23:54:21 +0100

    create Action9_Allow_ForAnyCardType_ForAnyCardStatus_NoMatterIfThereIsPIN

---
+++ CardsService.UnitTests/Action9PolicyTests.cs
@@ -0,0 +1,25 @@
+﻿using CardsService.Model;
+using CardsService.Policies;
+using CardsService.Services;
+
+namespace CardsService.UnitTests
+{
+    public class Action9PolicyTests
+    {
+        private readonly Action9Policy _action9policy = new Action9Policy();
+        private string _cardNumber = "12345";
+
+        public Action9PolicyTests()
+        {
+        }
+
+        [Theory]
+        [MemberData(nameof(CardsMatrixProvider.AllCardsCollection), MemberType = typeof(CardsMatrixProvider))]
+        public void Action9_Allow_ForAnyCardType_ForAnyCardStatus_NoMatterIfThereIsPIN(CardType cardType, CardStatus cardStatus, bool isPinSet)
+        {
+            var testedCard = new CardDetails(_cardNumber, cardType, cardStatus, isPinSet);
+
+            Assert.True(_action9policy.IsAllowed(testedCard));
+        }
+    }
+}
