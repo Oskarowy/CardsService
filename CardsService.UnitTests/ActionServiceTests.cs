dd293f704e1ded21ab71ceede29ad7e5659c2cb3
Oskar Białek <os.bialek@gmail.com>
2025-01-14 22:04:28 +0100

    red Action2_Allow_ForPrepaidCard_IfCardIsNotActive test created

--- CardsService.UnitTests/ActionServiceTests.cs
+++ CardsService.UnitTests/ActionServiceTests.cs
@@ -7,15 +7,17 @@ namespace CardsService.UnitTests
     public class ActionServiceTests
     {
         private readonly ActionService _actionService = new ActionService();
-        private readonly Action1Policy _policy = new Action1Policy();
+        private readonly Action1Policy _action1policy = new Action1Policy();
+        private readonly Action2Policy _action2policy = new Action1Policy();
 
+        #region Action1
         [Theory]
         [InlineData(CardStatus.Active)]
         public void Action1_Allow_ForPrepaidCard_IfCardIsActive(CardStatus cardStatus)
         {
             var cardDetails = new CardDetails("123", CardType.Prepaid, cardStatus, true);
 
-            Assert.True(_policy.IsAllowed(cardDetails));
+            Assert.True(_action1policy.IsAllowed(cardDetails));
         }
 
         [Theory]
@@ -29,13 +31,24 @@ namespace CardsService.UnitTests
         {
             var cardDetails = new CardDetails("123", CardType.Prepaid, cardStatus, true);
 
-            Assert.False(_policy.IsAllowed(cardDetails));
+            Assert.False(_action1policy.IsAllowed(cardDetails));
         }
 
         [Fact]
         public void ActionNameInPolicy_ShouldBeCorrect()
         {
-            Assert.Equal("ACTION1", _policy.ActionName);
+            Assert.Equal("ACTION1", _action1policy.ActionName);
         }
+        #endregion
+        #region Action2
+        [Theory]
+        [InlineData(CardStatus.Inactive)]
+        public void Action2_Allow_ForPrepaidCard_IfCardIsNotActive(CardStatus cardStatus)
+        {
+            var cardDetails = new CardDetails("123", CardType.Prepaid, cardStatus, true);
+
+            Assert.True(_action2policy.IsAllowed(cardDetails));
+        }
+        #endregion
     }
 }
