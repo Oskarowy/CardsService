7a412fd0fe16834d8e9763742bc366f6a26ae159
Oskar Białek <os.bialek@gmail.com>
2025-01-14 23:48:52 +0100

    action3 test - logic implementation

---
+++ CardsService/Policies/Action3Policy.cs
@@ -0,0 +1,14 @@
+﻿using CardsService.Services;
+
+namespace CardsService.Policies
+{
+    public class Action3Policy : IActionPolicy
+    {
+        public string ActionName => "ACTION3";
+
+        public bool IsAllowed(CardDetails cardDetails)
+        {
+            return true;
+        }
+    }
+}
