8c9329ac113428660b5b92114d8e1da4fb2bd35b
Oskar Białek <os.bialek@gmail.com>
2025-01-14 23:58:39 +0100

    created Action5_Allow_ForCreditCard_ForAnyCardStatus_NoMatterIfThereIsPIN test

---
+++ CardsService.UnitTests/Action5PolicyTests.cs
@@ -0,0 +1,27 @@
+﻿using CardsService.Model;
+using CardsService.Policies;
+using CardsService.Services;
+
+namespace CardsService.UnitTests
+{
+    public class Action5PolicyTests
+    {
+        private readonly Action5Policy _action5policy = new Action5Policy();
+        private string _cardNumber = "12345";
+
+        public Action5PolicyTests()
+        {
+        }
+
+        [Theory]
+        [MemberData(nameof(CardsMatrixProvider.AllCardsCollection), MemberType = typeof(CardsMatrixProvider))]
+        public void Action5_Allow_ForCreditCard_ForAnyCardStatus_NoMatterIfThereIsPIN(CardType cardType, CardStatus cardStatus, bool isPinSet)
+        {
+            if (cardType != CardType.Credit) return;
+
+            var testedCard = new CardDetails(_cardNumber, cardType, cardStatus, isPinSet);
+
+            Assert.True(_action5policy.IsAllowed(testedCard));
+        }
+    }
+}
