5dbc89d7b8c2626c19c204641bf8b7e6c3901ea1
Oskar Białek <os.bialek@gmail.com>
2025-01-14 21:37:26 +0100

    Red Action1_Deny_ForPrepaidCard_IfCardIsNotActive test created

--- CardsService.UnitTests/ActionServiceTests.cs
+++ CardsService.UnitTests/ActionServiceTests.cs
@@ -20,5 +20,23 @@ namespace CardsService.UnitTests
             Assert.Contains(action1name, allowedActions);
         }
 
+        [Theory]
+        [InlineData(CardStatus.Ordered)]
+        [InlineData(CardStatus.Inactive)]
+        [InlineData(CardStatus.Restricted)]
+        [InlineData(CardStatus.Blocked)]
+        [InlineData(CardStatus.Expired)]
+        [InlineData(CardStatus.Closed)]
+        public void Action1_Deny_ForPrepaidCard_IfCardIsNotActive(CardStatus cardStatus)
+        {
+            string action1name = "ACTION1";
+
+            var service = new ActionService();
+            var cardDetails = new CardDetails("123", CardType.Prepaid, cardStatus, true);
+
+            List<string> allowedActions = service.GetAllowedActions(cardDetails);
+
+            Assert.DoesNotContain(action1name, allowedActions);
+        }
     }
 }
