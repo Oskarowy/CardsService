e1ecb4b47e12e1954d524acef13a504506186924
Oskar Białek <os.bialek@gmail.com>
2025-01-15 01:17:55 +0100

    implemented Action7 policy logic

---
+++ CardsService/Policies/Action7Policy.cs
@@ -0,0 +1,26 @@
+﻿using CardsService.Model;
+using CardsService.Services;
+
+namespace CardsService.Policies
+{
+    public class Action7Policy : IActionPolicy
+    {
+        public string ActionName => "ACTION7";
+
+        public bool IsAllowed(CardDetails cardDetails)
+        {
+            var alwaysBlockedStatuses = new List<CardStatus> { CardStatus.Restricted, CardStatus.Expired, CardStatus.Closed };
+            var pinDependingStatuses = new List<CardStatus> { CardStatus.Active, CardStatus.Inactive, CardStatus.Ordered };
+
+            if (cardDetails.CardStatus == CardStatus.Blocked)
+                return cardDetails.IsPinSet;
+
+            if (alwaysBlockedStatuses.Contains(cardDetails.CardStatus)) return false;
+
+            if (pinDependingStatuses.Contains(cardDetails.CardStatus))
+                return !cardDetails.IsPinSet;
+
+            throw new NotImplementedException();
+        }
+    }
+}
