b6bb0fe616f219a0a95163215361005ab15e1389
Oskar Białek <os.bialek@gmail.com>
2025-01-15 20:20:08 +0100

    disabled FakePolicies being returned while testing ActionService (with real policies)

--- CardsService.UnitTests/PoliciesImplementationProvider.cs
+++ CardsService.UnitTests/PoliciesImplementationProvider.cs
@@ -20,7 +20,7 @@ namespace CardsService.UnitTests
 
             return assemblies
             .SelectMany(assembly => assembly.GetTypes())
-            .Where(type => interfaceType.IsAssignableFrom(type) && type.IsClass && !type.IsAbstract)
+            .Where(type => interfaceType.IsAssignableFrom(type) && type.IsClass && !type.IsAbstract && !type.Name.StartsWith("FakePolicy"))
             .Select(type => (IActionPolicy)Activator.CreateInstance(type))
             .ToList();
         }
