6a3f5ec40d576cce786e317d8b3eee42454d1439
Oskar Białek <os.bialek@gmail.com>
2025-01-15 01:20:26 +0100

    Action6 policy refactor

--- CardsService/Policies/Action6Policy.cs
+++ CardsService/Policies/Action6Policy.cs
@@ -7,14 +7,27 @@ namespace CardsService.Policies
     {
         public string ActionName => "ACTION6";
 
+        //public bool IsAllowed(CardDetails cardDetails)
+        //{
+        //    var alwaysBlockedStatuses = new List<CardStatus> { CardStatus.Restricted, CardStatus.Expired, CardStatus.Closed };
+        //    var pinDependingStatuses = new List<CardStatus> { CardStatus.Active, CardStatus.Inactive, CardStatus.Ordered };
+
+        //    if (cardDetails.CardStatus == CardStatus.Blocked)
+        //        return cardDetails.IsPinSet;
+
+        //    if (alwaysBlockedStatuses.Contains(cardDetails.CardStatus)) return false;
+
+        //    if (pinDependingStatuses.Contains(cardDetails.CardStatus))
+        //        return cardDetails.IsPinSet;
+
+        //    throw new NotImplementedException();
+        //}
+
         public bool IsAllowed(CardDetails cardDetails)
         {
             var alwaysBlockedStatuses = new List<CardStatus> { CardStatus.Restricted, CardStatus.Expired, CardStatus.Closed };
-            var pinDependingStatuses = new List<CardStatus> { CardStatus.Active, CardStatus.Inactive, CardStatus.Ordered };
-
-            if (cardDetails.CardStatus == CardStatus.Blocked)
-                return cardDetails.IsPinSet;
-
+            var pinDependingStatuses = new List<CardStatus> { CardStatus.Active, CardStatus.Inactive, CardStatus.Ordered, CardStatus.Blocked };
+            
             if (alwaysBlockedStatuses.Contains(cardDetails.CardStatus)) return false;
 
             if (pinDependingStatuses.Contains(cardDetails.CardStatus))
