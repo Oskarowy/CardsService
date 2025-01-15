accabf26a16e6da1e268ea5aab30355063e35b9b
Oskar Białek <os.bialek@gmail.com>
2025-01-15 18:39:49 +0100

    card details positive test

---
+++ CardsService.UnitTests/CardServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using CardsService.Model;
+using CardsService.Policies;
+using CardsService.Services;
+
+namespace CardsService.UnitTests
+{
+    public class CardServiceTests
+    {
+        private readonly CardService _cardService;
+
+        public CardServiceTests()
+        {
+            _cardService = new CardService();
+        }
+
+        [Fact]
+        public async Task ShouldReturnCardDetails_WhenCardExists()
+        {
+            CardType cardType = CardType.Prepaid;
+            CardStatus cardStatus = CardStatus.Active;
+            string cardNumber = "Card13";
+            string userId = "User1";
+            bool isPinSet = false;
+            var expectedCardDetails = new CardDetails(cardNumber, cardType, cardStatus, isPinSet);
+
+            var cardDetails = await _cardService.GetCardDetails(userId, cardNumber);
+            Assert.NotNull(cardDetails);
+            Assert.Equal(expectedCardDetails, cardDetails);
+        }
+    }
+}
